576b9d8dcab6927c58b56ed8fba216a3d1a52e70
Vishwas JS <vishwasjs2806@gmail.com>
2023-12-04 19:06:28 +0530

    some update

--- index.js
+++ index.js
@@ -69,3 +69,35 @@ let crntIndex = 0;
             showSlides(totalSlides - 1);
         }
     }
+
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+          },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          },
+      });
